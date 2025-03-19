6a66b24207c6a8270e5be1ff5b1ff9b16bf7cc57
Patrik söderberg <patrik@dbi.nu>
2025-03-19 18:01:33 +0100

    test

---
+++ client/src/Pages/homePage.jsx
@@ -0,0 +1,21 @@
+function homePage(){
+    return(
+        <>
+            <header id="navbar">
+                <div id="general-info">
+                    <p>logo</p>
+                    <h1>Namn</h1>
+                    <p>Min sida</p>
+                </div>
+                <div>
+
+                </div>
+            </header>
+            <div>
+                <h1>Hejsan</h1>
+            </div>
+        </>)
+}
+
+export default homePage;
+
